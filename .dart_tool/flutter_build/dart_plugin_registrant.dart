1de54a07d458c18954b5c63f826d4f05c08bd16f
Uniqkode <yungboox@gmail.com>
2023-08-29 03:54:45 +0100

    add submit document page

---
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e481a092c14a8ea618c60a706a680b1d753a6843e470dca37de5e419de0ae6f9
+size 9062
--- lib/pages/auth/submit_document.dart
+++ lib/pages/auth/submit_document.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51901b42cb239fb65b9f3cf0444e006bea75a2491ca0d48307398d08fc57dbd8
-size 13778
+oid sha256:d17ebe7b7f83ee636f8796eb3b96ac360f0bd72e4eaa25a1e34da1f1ebd4b39f
+size 15206
